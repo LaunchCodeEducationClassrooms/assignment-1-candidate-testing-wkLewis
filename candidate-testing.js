b505d1ce37056d7da762070c0c383a807941e9f0
Whitney Lewis <barclaywhitney@gmail.com>
2021-02-22 00:06:14 +0000

    changed names of grading variables

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,7 +8,7 @@ let candidateName = "";
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
-let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2*10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
+let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
 let correctAnswers = ["Sally Ride", "True", "40", "Trajectory", "3"];
 let candidateAnswers = [];
 
@@ -35,19 +35,19 @@ function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
 
+let correctCount = 0;
 let grade = 0;
-let totalGrade = 0;
 
 for (let i = 0; i < questions.length; i++) {
 
       if (correctAnswers[i].toLowerCase() === candidateAnswers[i].toLowerCase()) {
-        grade++;
+        correctCount++;
       }
   }
-  totalGrade = (grade / questions.length) * 100;
-  console.log(">>>> Overall Grade: " +`${totalGrade}`+ "% " + `${grade} ` + "of " + `${questions.length}` + " responses correct) <<<<");
+  grade = (correctCount / questions.length) * 100;
+  console.log(">>>> Overall Grade: " +`${grade}`+ "% " + `${correctCount} ` + "of " + `${questions.length}` + " responses correct) <<<<");
 
-      if (totalGrade >= 80) {
+      if (grade >= 80) {
         console.log(">>> Status: PASSED <<<");
       } else {
         console.log(">>> Status: FAILED <<<");
